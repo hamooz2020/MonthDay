1012584e5d49bafacb4c5e41d16dd31f0ae47859
Hamza <hamooz7670@gmail.com>
2019-11-14 09:18:12 -0500

    full intellij commit

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,19 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        int a,b,c,d;
+
+    while(true) {
+
+
+
+
+   }
+}
